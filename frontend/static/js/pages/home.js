38f88d46630146c245dce41b630b138d11fdf469
hyejin <hjhur004@gmail.com>
2022-01-06 01:17:49 +0900

    1/5 spa로 메인페이지 일부 구현

---
+++ frontend/static/js/pages/home.js
@@ -0,0 +1,10 @@
+export default class {
+    constructor() {
+        document.title = "Ajou_memo";
+    }
+    async getHtml() {
+        return `
+            <h1>This is Ajou memo main</h1>
+        `;
+    }
+}
